98e979e0cc071b1a36aa870cfb40a6e85b07c18d
yone098 <yone098@gmail.com>
2014-03-28 00:27:15 +0900

    bench add.

---
+++ bench.php
@@ -0,0 +1,22 @@
+<?php
+function loop_test() {
+  $sum = 0;
+  for ($i = 0; $i < 1000000; $i++) {
+    $sum += $i * 2;
+  }
+}
+
+function main() {
+  $startTime = microtime(true);
+  loop_test();
+  $endTime = microtime(true);
+  echo $endTime - $startTime . "秒\n";
+
+
+  $startTime = microtime(true);
+  loop_test();
+  $endTime = microtime(true);
+  echo $endTime - $startTime . "秒\n";
+}
+
+main();
